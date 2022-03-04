e2a81594467f9fd38bb7bbc5c962371c3aaca80f
吴俊康 <wujk03@vanke.com>
2022-03-04 17:34:09 +0800

    更新模版

---
+++ materials/blocks/区块模板/script/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	beforeCompile: (context) => {
+		console.log(context, 3333)
+		context.vscode.window.showErrorMessage('12134')
+		return { a: 1212 }
+	},
+	afterCompile: (constext) => {
+		console.log(constext, 3333)
+		console.log(__dirname, __filename, process.cwd())
+	},
+}
