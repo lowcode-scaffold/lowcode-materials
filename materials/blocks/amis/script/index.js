8c4b6adb146c84694b33570c89c9eec7af7a0ad6
wjkang <ruoxieme@gmail.com>
2023-06-25 20:46:18 +0800

    update

--- materials/blocks/amis/script/index.js
+++ materials/blocks/amis/script/index.js
@@ -6,6 +6,8 @@ module.exports = {
 	},
 	test: (context) => {
 		context.outputChannel.appendLine(Object.keys(context))
+		context.outputChannel.appendLine(JSON.stringify(context.model))
+		context.outputChannel.appendLine(context.params)
 		return { ...context.model, name: "测试一下", }
 	}
 };
