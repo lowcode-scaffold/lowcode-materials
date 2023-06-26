c1c7d4b7dc555341f217a0f393439aeb6df2d5cc
wjkang <ruoxieme@gmail.com>
2023-06-27 01:01:00 +0800

    update

--- materials/blocks/amis/script/index.js
+++ materials/blocks/amis/script/index.js
@@ -1,22 +1,24 @@
 require('ts-node').register({
 	transpileOnly: false,
-	emit: true,
-	compilerHost: true, // 和 emit 一起设置为 true，会在 .ts-node 文件夹输出编译后的代码
-	cwd: __dirname, // 要输出编译后代码必须配置，否则 mac 会报错 EROFS: read-only file system, mkdir '/.ts-node'
+	emit: false,
+	compilerHost: false, // 和 emit 一起设置为 true，会在 .ts-node 文件夹输出编译后的代码
+	cwd: __dirname, // 要输出编译后代码必须配置，否则会报错 EROFS: read-only file system, mkdir '/.ts-node'。不输出也要配置不然会出现各种奇奇怪怪的报错
 })
 const path = require('path')
 // 清除缓存，保证每次修改代码后实时生效
-delete require.cache[require.resolve(path.join(__dirname, 'title.ts'))]
-const title = require('./title')
+delete require.cache[require.resolve(path.join(__dirname, 'handle.ts'))]
+const title = require('./handle.ts')
 module.exports = {
-	beforeCompile: (context) => {},
-	afterCompile: (context) => {
+	beforeCompile: context => {
+		context.outputChannel.appendLine('compile amis start')
+	},
+	afterCompile: context => {
 		context.outputChannel.appendLine('compile amis end')
 	},
-	test: (context) => {
+	IntFromOcrText: (context) => {
 		context.outputChannel.appendLine(Object.keys(context))
 		context.outputChannel.appendLine(JSON.stringify(context.model))
-		context.outputChannel.appendLine(process.cwd())
+		context.outputChannel.appendLine(context.params)
 		context.outputChannel.appendLine(title.getTitle('lowcode'))
 		return { ...context.model, name: '测试一下' }
 	},
