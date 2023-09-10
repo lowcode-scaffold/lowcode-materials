cde53db9d4e9f9669c7a607d1d99b71eda2de34c
wjkang <ruoxieme@gmail.com>
2023-09-10 22:09:01 +0800

    update

---
+++ materials/blocks/现有模块中添加 antd vue table/script/index.js
@@ -0,0 +1,28 @@
+require('ts-node').register({
+  transpileOnly: true,
+  typeCheck: false,
+  emit: false,
+  compilerHost: false, // 和 emit 一起设置为 true，会在 .ts-node 文件夹输出编译后的代码
+  cwd: __dirname, // 要输出编译后代码必须配置，否则会报错 EROFS: read-only file system, mkdir '/.ts-node'。不输出也要配置不然会出现各种奇奇怪怪的报错
+});
+// 清除缓存，保证每次修改代码后实时生效，否则要重新打开 vscode
+const { clearCache } = require('../../../../share/clearCache.ts');
+
+clearCache(__dirname); // 调试的时候才打开，不然会很慢
+const main = require('./src/main.ts');
+const { context } = require('./src/context.ts');
+
+module.exports = {
+  beforeCompile: (lowcodeContext) => {
+    lowcodeContext.outputChannel.appendLine(
+      'compile start nest api srver start',
+    );
+  },
+  afterCompile: (lowcodeContext) => {
+    lowcodeContext.outputChannel.appendLine('compile start nest api srver end');
+  },
+  complete: (lowcodeContext) => {
+    context.lowcodeContext = lowcodeContext;
+    main.handleComplete();
+  },
+};
---
+++ materials/blocks/现有模块中添加 antd vue table/script/src/main.ts
@@ -0,0 +1,244 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import { context } from './context';
+
+export async function handleComplete() {
+  const createBlockPath = context.lowcodeContext?.createBlockPath;
+  if (createBlockPath) {
+    // #region 更新 api.ts 文件
+    const apiFileContent = fs
+      .readFileSync(path.join(createBlockPath, 'temp.api.ts'))
+      .toString();
+
+    let apiFileContentOld = '';
+    try {
+      apiFileContentOld = fs
+        .readFileSync(path.join(createBlockPath, 'api.ts').toString())
+        .toString();
+    } catch {}
+
+    fs.writeFileSync(
+      path.join(createBlockPath, 'api.ts'),
+      apiFileContentOld + apiFileContent,
+    );
+    fs.removeSync(path.join(createBlockPath, 'temp.api.ts'));
+    // #endregion
+
+    // #region 更新 model.ts 文件
+    const modelFileContent = fs
+      .readFileSync(path.join(createBlockPath, 'temp.model.ts'))
+      .toString();
+    let modelFileContentOld = fs
+      .readFileSync(path.join(createBlockPath, 'model.ts').toString())
+      .toString();
+
+    const keywords = [
+      '// lowcode-model-import-api',
+      '// lowcode-model-type',
+      '// lowcode-model-variable',
+      '// lowcode-model-return-variable',
+    ];
+    const modelSplitArr = modelFileContent.split(
+      new RegExp(keywords.join('|'), 'ig'),
+    );
+    const modelImportApi = modelSplitArr[1].replace(/\n/g, '');
+    const modelType = modelSplitArr[2];
+    const modelVariable = modelSplitArr[3];
+    const modelReturnVariable = modelSplitArr[4].replace(/\n/g, '');
+
+    if (!modelFileContentOld.includes('// lowcode-model-import-api')) {
+      modelFileContentOld = `// lowcode-model-import-api\n${modelFileContentOld}`;
+    }
+    modelFileContentOld = modelFileContentOld.replace(
+      '// lowcode-model-import-api',
+      modelImportApi,
+    );
+
+    if (!modelFileContentOld.includes('// lowcode-model-type')) {
+      modelFileContentOld = modelFileContentOld.replace(
+        'export const useModel',
+        '// lowcode-model-type\nexport const useModel',
+      );
+    }
+    modelFileContentOld = modelFileContentOld.replace(
+      '// lowcode-model-type',
+      modelType,
+    );
+
+    if (!modelFileContentOld.includes('// lowcode-model-variable')) {
+      modelFileContentOld = modelFileContentOld.replace(
+        'return {',
+        '// lowcode-model-variable\nreturn {',
+      );
+    }
+    modelFileContentOld = modelFileContentOld.replace(
+      '// lowcode-model-variable',
+      modelVariable,
+    );
+
+    if (!modelFileContentOld.includes('// lowcode-model-return-variable')) {
+      modelFileContentOld = modelFileContentOld.replace(
+        'return {',
+        'return {\n// lowcode-model-return-variable',
+      );
+    }
+    modelFileContentOld = modelFileContentOld.replace(
+      '// lowcode-model-return-variable',
+      modelReturnVariable,
+    );
+
+    fs.writeFileSync(
+      path.join(createBlockPath, 'model.ts'),
+      modelFileContentOld,
+    );
+    fs.removeSync(path.join(createBlockPath, 'temp.model.ts'));
+    // #endregion
+
+    // #region 更新 service.ts 文件
+    const serviceFileContent = fs
+      .readFileSync(path.join(createBlockPath, 'temp.service.ts').toString())
+      .toString();
+    let serviceFileContentOld = fs
+      .readFileSync(path.join(createBlockPath, 'service.ts').toString())
+      .toString()
+      .trim();
+
+    const serviceSplitArr = serviceFileContent.split(
+      new RegExp(
+        ['// lowcode-service-import-api', '// lowcode-service-method'].join(
+          '|',
+        ),
+        'ig',
+      ),
+    );
+    const serviceImportApi = serviceSplitArr[1].replace(/\n/g, '');
+    const serviceMethod = serviceSplitArr[2];
+    if (!serviceFileContentOld.includes('// lowcode-service-import-api')) {
+      serviceFileContentOld = `// lowcode-service-import-api\n${serviceFileContentOld}`;
+    }
+    serviceFileContentOld = serviceFileContentOld.replace(
+      '// lowcode-service-import-api',
+      serviceImportApi,
+    );
+
+    if (!serviceFileContentOld.includes('// lowcode-service-method')) {
+      serviceFileContentOld = `${serviceFileContentOld.slice(
+        0,
+        serviceFileContentOld.length - 1,
+      )}// lowcode-service-import-api\n}`;
+    }
+    serviceFileContentOld = serviceFileContentOld.replace(
+      '// lowcode-service-import-api',
+      serviceMethod,
+    );
+    fs.writeFileSync(
+      path.join(createBlockPath, 'service.ts'),
+      serviceFileContentOld,
+    );
+    fs.removeSync(path.join(createBlockPath, 'temp.service.ts'));
+
+    // #endregion
+
+    // #region 更新 presenter.tsx 文件
+    const presenterFileContent = fs
+      .readFileSync(path.join(createBlockPath, 'temp.presenter.tsx').toString())
+      .toString();
+    let presenterFileContentOld = fs
+      .readFileSync(path.join(createBlockPath, 'presenter.tsx').toString())
+      .toString();
+
+    const presenterSplitArr = presenterFileContent.split(
+      new RegExp(
+        [
+          '// lowcode-presenter-handlePageChange',
+          '// lowcode-presenter-return-handlePageChange',
+        ].join('|'),
+        'ig',
+      ),
+    );
+    const presenterMethod = presenterSplitArr[1];
+    const presenterReturnMethod = presenterSplitArr[2];
+
+    if (
+      !presenterFileContentOld.includes('// lowcode-presenter-handlePageChange')
+    ) {
+      presenterFileContentOld = presenterFileContentOld.replace(
+        'return {',
+        `// lowcode-presenter-handlePageChange\nreturn {`,
+      );
+    }
+    presenterFileContentOld = presenterFileContentOld.replace(
+      '// lowcode-presenter-handlePageChange',
+      presenterMethod,
+    );
+
+    if (
+      !presenterFileContentOld.includes(
+        '// lowcode-presenter-return-handlePageChange',
+      )
+    ) {
+      presenterFileContentOld = presenterFileContentOld.replace(
+        'return {',
+        'return {\n// lowcode-presenter-return-handlePageChange',
+      );
+    }
+    presenterFileContentOld = presenterFileContentOld.replace(
+      '// lowcode-presenter-return-handlePageChange',
+      presenterReturnMethod,
+    );
+    fs.writeFileSync(
+      path.join(createBlockPath, 'presenter.tsx'),
+      presenterFileContentOld,
+    );
+    fs.removeSync(path.join(createBlockPath, 'temp.presenter.tsx'));
+    // #endregion
+
+    // #region 更新 index.vue 文件
+    const vueFileContent = fs
+      .readFileSync(path.join(createBlockPath, 'temp.index.vue').toString())
+      .toString();
+
+    let vueFileContentOld = fs
+      .readFileSync(path.join(createBlockPath, 'index.vue').toString())
+      .toString();
+
+    const vueSplitArr = vueFileContent.split(
+      new RegExp(
+        ['<!-- lowcode-vue-template -->', '// lowcode-vue-columns'].join('|'),
+        'ig',
+      ),
+    );
+    const vueTemplate = vueSplitArr[1];
+    const vueColumns = vueSplitArr[2];
+
+    if (!vueFileContentOld.includes('<!-- lowcode-vue-template -->')) {
+      const index = vueFileContentOld.lastIndexOf('</template>');
+      vueFileContentOld = `${vueFileContentOld.substring(
+        0,
+        index,
+      )}<!-- lowcode-vue-template -->${vueFileContentOld.substring(index)}`;
+    }
+    vueFileContentOld = vueFileContentOld.replace(
+      '<!-- lowcode-vue-template -->',
+      vueTemplate,
+    );
+
+    if (!vueFileContentOld.includes('// lowcode-vue-columns')) {
+      vueFileContentOld = vueFileContentOld.replace(
+        '</script>',
+        `// lowcode-vue-columns\n</script>`,
+      );
+    }
+    vueFileContentOld = vueFileContentOld.replace(
+      '// lowcode-vue-columns',
+      vueColumns,
+    );
+
+    fs.writeFileSync(
+      path.join(createBlockPath, 'index.vue'),
+      vueFileContentOld,
+    );
+    fs.removeSync(path.join(createBlockPath, 'temp.index.vue'));
+    // #endregion
+  }
+}
