e8046533eb628f4c1a48ccc0be0dff67dd36503f
wjkang <ruoxieme@gmail.com>
2024-01-02 18:19:10 +0800

    ✨ feat: 测试使用 jsx 作为模版引擎

--- materials/blocks/测试使用 jsx 作为模版引擎/src/model1.ts.template.tsx
+++ materials/blocks/测试使用 jsx 作为模版引擎/src/model1.ts.template.tsx
@@ -10,36 +10,31 @@ interface IProps {
 
 const Content: React.FC<IProps> = (props) => (
   <>
-    import {'{'} reactive, ref {'}'} from "vue";
-    {props.api && (
-      <>
-        import {'{'} I
-        {props.funcName.slice(0, 1).toUpperCase() + props.funcName.slice(1)}
-        Result {'}'} from "./api";
-      </>
-    )}
+    {`import { reactive, ref } from "vue";`}
+    {props.api &&
+      `import { I${
+        props.funcName.slice(0, 1).toUpperCase() + props.funcName.slice(1)
+      }Result } from "./api";`}
+    {!props.api &&
+      `import { I${
+        props.fetchName.slice(0, 1).toUpperCase() + props.fetchName.slice(1)
+      }Result } from "./api";`}
     {!props.api && (
       <>
-        import {'{'} I
-        {props.fetchName.slice(0, 1).toUpperCase() + props.fetchName.slice(1)}
-        Result {'}'} from "./api";
-      </>
-    )}
-    {!props.api && (
-      <>
-        interface ITableListItem {'{'}
+        {`interface ITableListItem {`}
         {props.columns.map((item, index) => (
           <>
-            /** * {item.title}
-            */
+            {`/** 
+						* ${item.title}
+				    */
+					 `}
             {item.key || `column${index + 1}`}: string;
           </>
         ))}
-        {'}'}
+        {`}`}
       </>
     )}
-    <></>
-    export type Model = ReturnType {'<'}typeof useModel{'>'};
+    {`export type Model = ReturnType <typeof useModel>;`}
   </>
 );
 
