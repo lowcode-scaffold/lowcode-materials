b998822d72b00b2d2779371df3cebafe43e32e20
wjkang <ruoxieme@gmail.com>
2024-02-29 18:02:24 +0800

    ✨ feat: uTools 自动化脚本测试

---
+++ uTools.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs-extra');
+/**
+ * @description
+ * @param {string} dirPath
+ * @return {string[]}
+ */
+function getAllFiles(dirPath) {
+  const files = fs.readdirSync(dirPath);
+
+  let result = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const file of files) {
+    const filePath = `${dirPath}/${file}`;
+    // eslint-disable-next-line no-await-in-loop
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      result = result.concat(getAllFiles(filePath));
+    } else {
+      result.push(filePath);
+    }
+  }
+
+  return result;
+}
+
+getAllFiles(path.join(__dirname, 'dist', 'uTools'))
+  .filter((s) => s.includes('index.js'))
+  .forEach((file) => {
+    const mainFilePath = file
+      .replace(/\\/g, '/')
+      .replace('index.js', 'src/main');
+    const content = fs.readFileSync(file).toString();
+    const newContent = content
+      .replace(
+        'Object.defineProperty(exports, "__esModule", { value: true });',
+        '',
+      )
+      .replace(
+        'const main_1 = require("./src/main");',
+        `const main_1 = require("${mainFilePath}");`,
+      );
+    fs.writeFileSync(file, newContent);
+  });
---
+++ uTools/测试/src/main.ts
@@ -0,0 +1,3 @@
+export const bootstrap = () => {
+  utools.showNotification('Hello Word');
+};
