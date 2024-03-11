44e4986dc41759a8915b2b0dcdfbab82c8ba2ba8
wjkang <ruoxieme@gmail.com>
2024-03-11 17:53:23 +0800

    feat(uTools): Ask Groq

---
+++ uTools/Ask Groq/script/src/main.ts
@@ -0,0 +1,22 @@
+import { clipboard } from 'electron';
+
+export const bootstrap = async () => {
+  utools.setUBrowserProxy({ proxyRules: 'http://127.0.0.1:7890' });
+  utools.ubrowser
+    // .devTools('bottom')
+    .useragent('Chrome')
+    .goto('https://groq.com/')
+    .wait('#model-selector')
+    .wait('#chat')
+    .when('#chat')
+    .wait(1000)
+    .focus('#chat')
+    .wait(300)
+    .paste('你好')
+    .wait(1000)
+    .press('Enter')
+    .end()
+    .run({});
+  utools.outPlugin();
+  utools.hideMainWindow();
+};
---
+++ uTools/Ask Kimi/script/src/main.ts
@@ -0,0 +1,17 @@
+import { clipboard } from 'electron';
+
+export const bootstrap = async () => {
+  utools.ubrowser
+    .goto('https://kimi.moonshot.cn/')
+    .wait('div[role="textbox"]')
+    .focus('div[role="textbox"]')
+    .paste('你好')
+    .wait(300)
+    .press('Enter')
+    .run({})
+    .catch((err) => {
+      utools.showNotification(err.message);
+    });
+  utools.outPlugin();
+  utools.hideMainWindow();
+};
