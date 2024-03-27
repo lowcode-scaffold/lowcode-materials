cc4d39eee1e53c7f14bd7707ac876a5d8917cfed
wjkang <ruoxieme@gmail.com>
2024-03-27 23:30:41 +0800

    feat(uTools): Open Tldraw

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,7 @@
 const { build } = require('esbuild');
 
 const sharedConfig = {
-  entryPoints: ['./dist/uTools/abcdef/script/index.js'],
+  entryPoints: ['./dist/uTools/翻译为驼峰格式/script/src/main.js'],
   bundle: true,
   minify: false,
   // only needed if you have dependencies
---
+++ uTools/Open Tldraw/script/src/main.ts
@@ -0,0 +1,33 @@
+import { askChatGPT as askOpenai } from '../../../../share/utils/uTools';
+
+export const bootstrap = async (scriptFile?: string) => {
+  utools.redirect(['lowcode', 'lowcode'], {
+    type: 'text',
+    data: JSON.stringify({ scriptFile, route: '/tldraw' }),
+  });
+};
+
+type LLMMessage = (
+  | {
+      role: 'system';
+      content: string;
+    }
+  | {
+      role: 'user';
+      content:
+        | string
+        | (
+            | {
+                type: 'image_url';
+                image_url: { url: string };
+              }
+            | { type: 'text'; text: string }
+          )[];
+    }
+)[];
+
+// 给页面调用的
+export const askChatGPT = (data: {
+  messages: LLMMessage;
+  handleChunk: (chunck: string) => void;
+}) => askOpenai({ ...data, hostname: 'api.chatanywhere.com.cn' });
